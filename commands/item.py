1b02b9d1eaa4dd2a31f9cc39c959745c4cbb8c25
David Reichard <dmreichard@gmail.com>
2019-04-06 23:33:34 -0400

    Override default get/drop/give commands.

---
+++ commands/item.py
@@ -0,0 +1,158 @@
+"""
+Item related commands module.
+
+"""
+from evennia import CmdSet
+from evennia.commands.default.muxcommand import MuxCommand
+
+
+class ItemCmdSet(CmdSet):
+    """ Command set for item related commands. """
+    key = 'harvest_cmdset'
+    priority = 1
+
+    def at_cmdset_creation(self):
+        self.add(CmdGet())
+        self.add(CmdDrop())
+        self.add(CmdGive())
+
+
+class CmdGet(MuxCommand):
+    """
+    pick up something
+
+    Usage:
+      get <obj>
+
+    Picks up an object from your location and puts it in
+    your inventory.
+    """
+    key = "get"
+    aliases = "grab"
+    locks = "cmd:all()"
+    arg_regex = r"\s|$"
+
+    def func(self):
+        """implements the command."""
+
+        caller = self.caller
+
+        if not self.args:
+            caller.msg("Get what?")
+            return
+        obj = caller.search(self.args, location=caller.location)
+        if not obj:
+            return
+        if caller == obj:
+            caller.msg("You can't get yourself.")
+            return
+        if not obj.access(caller, 'get'):
+            if obj.db.get_err_msg:
+                caller.msg(obj.db.get_err_msg)
+            else:
+                caller.msg("You can't get that.")
+            return
+
+        # calling at_before_get hook method
+        if not obj.at_before_get(caller):
+            return
+
+        obj.move_to(caller, quiet=True)
+        caller.msg("You pick up %s." % obj.name)
+        caller.location.msg_contents("%s picks up %s." %
+                                     (caller.name,
+                                      obj.name),
+                                     exclude=caller)
+        # calling at_get hook method
+        obj.at_get(caller)
+
+
+class CmdDrop(MuxCommand):
+    """
+    drop something
+
+    Usage:
+      drop <obj>
+
+    Lets you drop an object from your inventory into the
+    location you are currently in.
+    """
+
+    key = "drop"
+    locks = "cmd:all()"
+    arg_regex = r"\s|$"
+
+    def func(self):
+        """Implement command"""
+
+        caller = self.caller
+        if not self.args:
+            caller.msg("Drop what?")
+            return
+
+        # Because the DROP command by definition looks for items
+        # in inventory, call the search function using location = caller
+        obj = caller.search(self.args, location=caller,
+                            nofound_string="You aren't carrying %s." % self.args,
+                            multimatch_string="You carry more than one %s:" % self.args)
+        if not obj:
+            return
+
+        # Call the object script's at_before_drop() method.
+        if not obj.at_before_drop(caller):
+            return
+
+        obj.move_to(caller.location, quiet=True)
+        caller.msg("You drop %s." % (obj.name,))
+        caller.location.msg_contents("%s drops %s." %
+                                     (caller.name, obj.name),
+                                     exclude=caller)
+        # Call the object script's at_drop() method.
+        obj.at_drop(caller)
+
+
+class CmdGive(MuxCommand):
+    """
+    give away something to someone
+
+    Usage:
+      give <inventory obj> <to||=> <target>
+
+    Gives an items from your inventory to another character,
+    placing it in their inventory.
+    """
+    key = "give"
+    rhs_split = ("=", " to ")  # Prefer = delimiter, but allow " to " usage.
+    locks = "cmd:all()"
+    arg_regex = r"\s|$"
+
+    def func(self):
+        """Implement give"""
+
+        caller = self.caller
+        if not self.args or not self.rhs:
+            caller.msg("Usage: give <inventory object> = <target>")
+            return
+        to_give = caller.search(self.lhs, location=caller,
+                                nofound_string="You aren't carrying %s." % self.lhs,
+                                multimatch_string="You carry more than one %s:" % self.lhs)
+        target = caller.search(self.rhs)
+        if not (to_give and target):
+            return
+        if target == caller:
+            caller.msg("You keep %s to yourself." % to_give.key)
+            return
+        if not to_give.location == caller:
+            caller.msg("You are not holding %s." % to_give.key)
+            return
+
+        # calling at_before_give hook method
+        if not to_give.at_before_give(caller, target):
+            return
+
+        # give object
+        caller.msg("You give %s to %s." % (to_give.key, target.key))
+        to_give.move_to(target, quiet=True)
+        target.msg("%s gives you %s." % (caller.key, to_give.key))
+        # Call the object script's at_give() method.
+        to_give.at_give(caller, target)
