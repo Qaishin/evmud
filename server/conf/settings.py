67c7fa6630b8416b6f600f2ce164ff8dd4da69d1
David Reichard <dmreichard@gmail.com>
2019-04-10 23:00:05 -0400

    Changed player starting location.

--- server/conf/settings.py
+++ server/conf/settings.py
@@ -34,6 +34,7 @@ from evennia.settings_default import *
 # This is the name of your game. Make it catchy!
 SERVERNAME = "mud"
 
+START_LOCATION = "#9"
 
 ######################################################################
 # Settings given in secret_settings.py override those in this file.
